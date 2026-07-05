5d1c701a60a1c11a10066797b96d3ac95dc00611
Chintan-code10 <chintandarji10@gmail.com>
2026-07-05 23:14:06 +0530

    Corrected landing page

--- vault-map-integration/src/main/java/com/veeva/vault/integration/controller/VaultController.java
+++ vault-map-integration/src/main/java/com/veeva/vault/integration/controller/VaultController.java
@@ -1,16 +1,10 @@
 package com.veeva.vault.integration.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,7 +17,7 @@ public class VaultController {
 	public String session(Model model) {
 		logger.info("In controller");
 		try {
-			return "HOME";
+			return "home";
 
 		} catch (Exception e) {
 			model.addAttribute("error", "Unable to load study map. Please try again later.");
@@ -31,20 +25,4 @@ public class VaultController {
 		}
 	}
 
-	@GetMapping("")
-	public Object showStudyMap(Model model, @RequestHeader(value = "X-Session-Id", required = false) String sessionId) {
-		if (sessionId == null || sessionId.isBlank()) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid session");
-		}
-		try {
-//			List<Map<String, Object>> availableStudies = mapDataService.getAvailableStudies(sessionId);
-//			model.addAttribute("availableStudies", availableStudies);
-			logger.info("Displaying study map page with available studies");
-			return "study-map";
-		} catch (Exception e) {
-			logger.error("Error loading study map page", e);
-			model.addAttribute("error", "Unable to load study map. Please try again later.");
-			return "error";
-		}
-	}
 }
